87ccfa9537fcd6f5e0d61f20b8daf71c55b5c769
Joe Park-Kennaby <joeparkkennaby@gmail.com>
2022-05-03 23:07:22 +0100

    modules - setup BookmarkModule (generated by the CLI)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
+import { BookmarkModule } from './bookmark/bookmark.module';
 
 @Module({
   imports: [
     AuthModule,
-    UserModule
+    UserModule,
+    BookmarkModule
   ],
 })
 export class AppModule { }
