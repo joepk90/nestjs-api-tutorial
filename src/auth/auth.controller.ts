a65787751abc0f4ca5143dbcf46d569f77cfbeb1
Joe Park-Kennaby <joeparkkennaby@gmail.com>
2022-05-04 13:40:35 +0100

    Auth controller - setup auth routes: signup, signin

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,11 +1,23 @@
-import { Controller } from "@nestjs/common";
+import { Controller, Post } from "@nestjs/common";
 import { AuthService } from './auth.service';
 
-@Controller({})
+@Controller('auth')
 export class AuthController {
 
     // dependancy injection: inject the AuthService module
     constructor(private AuthService: AuthService) {
-
     }
+
+    // POST /auth/signup/
+    @Post('signup')
+    signup() {
+        return 'Signed Up'
+    }
+
+    // POST /auth/signin/
+    @Post('signin')
+    signin() {
+        return 'Signed In'
+    }
+
 }
