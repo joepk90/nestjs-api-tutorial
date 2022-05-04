e8e5be42beea1964c08245bd8beeedfd12607870
Joe Park-Kennaby <joeparkkennaby@gmail.com>
2022-05-04 18:19:06 +0100

    Nestjs global pipes - add whitelist parameter to global ValidationPipe config - prevent unvalidated paramters in the request body

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true // removes unvalidated attributes being send (i.e. if an id is posted but not expected it will be removed)
+  }));
   await app.listen(4000);
 }
 bootstrap();
