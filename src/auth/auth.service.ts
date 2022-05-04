2933cdb8ceff1356a825ef088e974a7f57040a9a
Joe Park-Kennaby <joeparkkennaby@gmail.com>
2022-05-04 18:27:21 +0100

    Sign up logic - import argin2... AMEND THIS COMMIT)

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from "@nestjs/common";
 import { PrismaService } from '../prisma/prisma.service';
 import { AuthDto } from "./dto";
+import * as argon from 'argon2';
 
 @Injectable({})
 export class AuthService {
@@ -9,6 +10,11 @@ export class AuthService {
 
     signup(dto: AuthDto) {
         // return { msg: 'Signed Up' }; // nestjs will handle returned data type for you.
+
+        // TODO
+        // generate the password hash
+        // save the new user in the db
+        // return saved user
         return 'Signed Up'
     }
 
