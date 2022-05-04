1c70c0e0a111bc24a0891acc72fbcb66e4e251fe
Joe Park-Kennaby <joeparkkennaby@gmail.com>
2022-05-04 17:23:02 +0100

    Prisma module - setup the the PrismaModule to export the PrismaService. import the PrismaService in the AuthModule and instantiate in the AuthService

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,10 @@
 import { Injectable } from "@nestjs/common";
+import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable({})
 export class AuthService {
+
+    constructor(private prismaService: PrismaService) { }
 
     signup() {
         // return { msg: 'Signed Up' }; // nestjs will handle returned data type for you.
